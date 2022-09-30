86ca356b3251e8dd0acb7fa2eb76441dd3b32d41
Manan <manan228@yahoo.in>
2022-09-30 20:29:21 +0530

    Added react-router-dom, login page and its error handling
    Added react-router-dom V6, configured react-router-dom, Login page and its error handling and also created path for signup and login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUp from "./SignUp/SignUp";
+import Login from "./Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Login/Login.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const onLoginClickHandler = async (e) => {
+    e.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    const loginObj = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/login",
+        loginObj
+      );
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+      if (err.response.status === 404) {
+        setError("User does not exist");
+      }
+
+      if (err.response.status === 401) {
+        setError("Incorrect Password");
+      }
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={onLoginClickHandler}>
+        <div>
+          <label>Email</label>
+          <input type="email" ref={emailInputRef} required />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" ref={passwordInputRef} required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <NavLink to="/signup">New User? SignUp</NavLink>
+      {error ? <div>{error}</div> : null}
+    </>
+  );
+};
+
+export default Login;
